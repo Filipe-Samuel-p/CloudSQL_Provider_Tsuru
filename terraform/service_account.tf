533763c6085503108548f491104990cce7eac0c6
Filipe-Samuel-p <felipepires.p125@gmail.com>
2026-08-01 16:29:26 -0300

    feat: create a new Service Account

---
+++ terraform/service_account.tf
@@ -0,0 +1,15 @@
+resource "google_service_account" "sa_cloudsql" {
+  account_id   = "sa-cloud-sql-provider"
+  display_name = "sa-cloudsql"
+  description  = "Service Account para se comunicar com a API do Cloud SQL"
+  project      = var.project_id
+}
+
+resource "google_project_iam_member" "cloudsql_admin_permission" {
+  depends_on = [google_service_account.sa_cloudsql]
+  project    = "treinamento-gcp-482202"
+  role       = "roles/cloudsql.admin"
+  member     = "serviceAccount:${google_service_account.sa_cloudsql.email}"
+}
+
+
---
+++ terraform/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "treinamento-gcp-482202"
+region     = "US-CENTRAL1"
